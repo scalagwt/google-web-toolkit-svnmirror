5870e920825322d77efbc9075bed3b9ca45efadd
gwt.team.tobyr <gwt.team.tobyr@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2007-02-07 23:58:37 +0000

    Updated profiler to work with the new compiler changes
    git-svn-id: http://google-web-toolkit.googlecode.com/svn/branches/opt_imgs_and_startup@384 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- dev/core/src/com/google/gwt/dev/jjs/impl/ProfileInstrumenter.java
+++ dev/core/src/com/google/gwt/dev/jjs/impl/ProfileInstrumenter.java
@@ -26,6 +26,8 @@ import com.google.gwt.dev.jjs.ast.JStatement;
 import com.google.gwt.dev.jjs.ast.JStringLiteral;
 import com.google.gwt.dev.jjs.ast.JTryStatement;
 import com.google.gwt.dev.jjs.ast.JVisitor;
+import com.google.gwt.dev.jjs.ast.Context;
+import com.google.gwt.dev.jjs.ast.JSourceInfo;
 
 import java.util.Collections;
 import java.util.Iterator;
@@ -33,107 +35,115 @@ import java.util.List;
 
 /**
  * Instruments the program with calls to the profiler API.
- *
  */
 public class ProfileInstrumenter {
-//
-//  public static void exec(JProgram program) {
-//    new ProfileInstrumenter(program).execImpl();
-//  }
-//
-//  private final JProgram program;
-//  private JMethod profilerEnterMethod;
-//  private JMethod profilerExitMethod;
-//
-//  private class MethodVisitor extends JVisitor {
-//
-//    private final ChangeList changeList = new ChangeList(
-//        "Instrument methods with calls to the profiler." );
-//
-//    public void endVisit(JMethod method) {
-//      super.endVisit(method);
-//    }
-//
-//    public ChangeList getChangeList() {
-//      return changeList;
-//    }
-//
-//    public boolean visit(JMethod method) {
-//
-//      // TODO(tobyr): implement native methods
-//      if ( method.isAbstract() || method.isNative() ) {
-//        return super.visit(method);
-//      }
-//
-//      JReferenceType enclosingType = method.getEnclosingType();
-//      JLiteral typeName = enclosingType == null ? program.getLiteralNull() : (JLiteral) getStringLiteral( enclosingType.getName() );
-//
-//      JStringLiteral methodName = getStringLiteral(method.getName());
-//      JStringLiteral methodSignature = getStringLiteral(method.toString());
-//
-//      JBlock newBody = new JBlock(program);
-//
-//      JMethodCall enterCall = new JMethodCall(program, null, profilerEnterMethod);
-//      enterCall.args.add(typeName);
-//      enterCall.args.add(methodName);
-//      enterCall.args.add(methodSignature);
-//
-//      JMethodCall exitCall = new JMethodCall(program, null, profilerExitMethod);
-//      exitCall.args.add(typeName);
-//      exitCall.args.add(methodName);
-//      exitCall.args.add(methodSignature);
-//
-//      JStatement enterStmt = new JExpressionStatement(program, enterCall);
-//      JStatement exitStmt = new JExpressionStatement(program, exitCall);
-//
-//      JBlock tryBlock = new JBlock(program);
-//      tryBlock.statements.addAll( method.body.statements );
-//      JBlock finallyBlock = new JBlock(program);
-//      finallyBlock.statements.add(exitStmt);
-//
-//      JTryStatement tryStatement = new JTryStatement( program, tryBlock, Collections.EMPTY_LIST,
-//        Collections.EMPTY_LIST, finallyBlock );
-//
-//      newBody.statements.add(enterStmt);
-//      newBody.statements.add( tryStatement );
-//      method.body.statements.clear();
-//      method.body.statements.addAll(newBody.statements);
-//
-//      return super.visit(method);
-//    }
-//
-//    private JStringLiteral getStringLiteral( String str ) {
-//      char[] chars = new char[ str.length() ];
-//      str.getChars( 0, str.length(), chars, 0 );
-//      return program.getLiteralString( chars );
-//    }
-//  }
-//
-//  private ProfileInstrumenter(JProgram program) {
-//    this.program = program;
-//
-//    // JReferenceType profiler = program.getFromTypeMap( "com.google.gwt.core.client.Profiler" );
-//    JReferenceType profiler = program.getSpecialProfiler();
-//    profilerEnterMethod = getMethod( profiler.methods, "methodEntered" );
-//    profilerExitMethod = getMethod( profiler.methods, "methodExited" );
-//  }
-//
-//  private JMethod getMethod( List methods, String name ) {
-//    for ( Iterator it = methods.iterator(); it.hasNext(); ) {
-//      JMethod method = (JMethod) it.next();
-//      if ( method.getName().equals( name ) ) {
-//        return method;
-//      }
-//    }
-//    throw new RuntimeException( "Unable to find the method com.google.gwt.core.client.Profiler." + name );
-//  }
-//
-//  private void execImpl() {
-//    MethodVisitor visitor = new MethodVisitor();
-//    program.traverse(visitor);
-//    ChangeList changes = visitor.getChangeList();
-//    if (!changes.empty()) {
-//      changes.apply();
-//    }
-//  }
+
+  private class MethodVisitor extends JVisitor {
+
+    private boolean didChange = false;
+
+    public boolean didChange() {
+      return didChange;
+    }
+
+    public void endVisit(JMethod method, Context ctx) {
+      super.endVisit(method, ctx);
+    }
+
+    public boolean visit(JMethod method, Context ctx) {
+      // TODO(tobyr): implement native methods
+      if (method.isAbstract() || method.isNative()) {
+        return super.visit(method, ctx);
+      }
+
+      JSourceInfo sourceInfo = method.getSourceInfo();
+
+      JReferenceType enclosingType = method.getEnclosingType();
+      JLiteral typeName = enclosingType == null ? program.getLiteralNull()
+          : (JLiteral) getStringLiteral(enclosingType.getName());
+
+      JStringLiteral methodName = getStringLiteral(method.getName());
+      JStringLiteral methodSignature = getStringLiteral(method.toString());
+
+      JBlock newBody = new JBlock(program, sourceInfo);
+
+      JMethodCall enterCall = new JMethodCall(program, sourceInfo, null,
+          profilerEnterMethod);
+      enterCall.getArgs().add(typeName);
+      enterCall.getArgs().add(methodName);
+      enterCall.getArgs().add(methodSignature);
+
+      JMethodCall exitCall = new JMethodCall(program, sourceInfo, null,
+          profilerExitMethod);
+      exitCall.getArgs().add(typeName);
+      exitCall.getArgs().add(methodName);
+      exitCall.getArgs().add(methodSignature);
+
+      JStatement enterStmt = new JExpressionStatement(program, sourceInfo,
+          enterCall);
+      JStatement exitStmt = new JExpressionStatement(program, sourceInfo,
+          exitCall);
+
+      JBlock tryBlock = new JBlock(program, sourceInfo);
+      tryBlock.statements.addAll(method.body.statements);
+      JBlock finallyBlock = new JBlock(program, sourceInfo);
+      finallyBlock.statements.add(exitStmt);
+
+      JTryStatement tryStatement = new JTryStatement(program, sourceInfo,
+          tryBlock, Collections.EMPTY_LIST,
+          Collections.EMPTY_LIST, finallyBlock);
+
+      newBody.statements.add(enterStmt);
+      newBody.statements.add(tryStatement);
+      method.body.statements.clear();
+      method.body.statements.addAll(newBody.statements);
+
+      didChange = true;
+
+      return super.visit(method, ctx);
+    }
+
+    private JStringLiteral getStringLiteral(String str) {
+      char[] chars = new char[ str.length() ];
+      str.getChars(0, str.length(), chars, 0);
+      return program.getLiteralString(chars);
+    }
+  }
+
+  public static void exec(JProgram program) {
+    new ProfileInstrumenter(program).execImpl();
+  }
+
+  private final JProgram program;
+
+  private JMethod profilerEnterMethod;
+
+  private JMethod profilerExitMethod;
+
+  private ProfileInstrumenter(JProgram program) {
+    this.program = program;
+
+    // JReferenceType profiler = program.getFromTypeMap( "com.google.gwt.core.client.Profiler" );
+    JReferenceType profiler = program.getSpecialProfiler();
+    profilerEnterMethod = getMethod(profiler.methods, "methodEntered");
+    profilerExitMethod = getMethod(profiler.methods, "methodExited");
+  }
+
+  private boolean execImpl() {
+    MethodVisitor visitor = new MethodVisitor();
+    visitor.accept(program);
+    return visitor.didChange;
+  }
+
+  private JMethod getMethod(List methods, String name) {
+    for (Iterator it = methods.iterator(); it.hasNext();) {
+      JMethod method = (JMethod) it.next();
+      if (method.getName().equals(name)) {
+        return method;
+      }
+    }
+    throw new RuntimeException(
+        "Unable to find the method com.google.gwt.core.client.Profiler."
+            + name);
+  }
 }
