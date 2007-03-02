1d54c6b1dfb7f1a1898507a5e9e3eafe49bb1620
gwt.team.ecc <gwt.team.ecc@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2007-03-02 13:04:46 +0000

    Removed ability to sub in custom buttons in return for cleaner inheritance
    git-svn-id: http://google-web-toolkit.googlecode.com/svn/branches/opt_imgs_and_startup@546 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/user/client/ui/SelectablePopupButton.java
+++ user/src/com/google/gwt/user/client/ui/SelectablePopupButton.java
@@ -19,13 +19,16 @@ package com.google.gwt.user.client.ui;
 import com.google.gwt.user.client.ui.SelectablePopup.ItemController;
 
 /**
- * Drop down push button. Wrapper for a <code>ComplexButton</code> with a built
- * in <code>SelectablePopup</code>.
+ * {@link CustomButton} with a selectable popup.
  */
-public class SelectablePopupButton extends Composite implements
+public class SelectablePopupButton extends CustomButton implements
     SourcesChangeEvents {
+
+  /**
+   * Default listener class.
+   */
   private class MyListener extends KeyboardListenerAdapter {
-    StringBuffer accum = new StringBuffer();
+    private StringBuffer accum = new StringBuffer();
 
     public void onKeyDown(Widget sender, char keyCode, int modifiers) {
       if (popup.isAttached() == false) {
@@ -54,65 +57,37 @@ public class SelectablePopupButton extends Composite implements
   }
 
   private SelectablePopup popup;
-  private ComplexButton button;
-
   private MyListener listener;
-
   private ItemController controller;
+
+  /**
+   * 
+   * Constructor for <code>DropDownPushButton</code>.
+   * 
+   * @param popup selectable popup
+   */
+  public SelectablePopupButton(SelectablePopup popup) {
+    this.popup = popup;
+    setToggleBehavior(true);
+    addDefaultListeners();
+  }
 
   /**
    * 
    * Constructor for <code>SelectablePopupButton</code>.
    * 
-   * @param button push button
-   * @param popup selectable popup
+   * @param upImage up image for button
+   * @param downImage down image for button
    */
-  public SelectablePopupButton(final ComplexButton button, final SelectablePopup popup) {
-    initWidget(button);
-    this.popup = popup;
-    this.button = button;
-
-    button.addClickListener(new ClickListener() {
-
-      public void onClick(Widget sender) {
-        showPopup();
-      }
-
-    });
-
-    popup.addPopupListener(new PopupListener() {
-      public void onPopupClosed(PopupPanel sender, boolean autoClosed) {
-        button.setPressed(false);
-        listener.accum.setLength(0);
-      }
-    });
-
-    button.addFocusListener(new FocusListener() {
-
-      public void onFocus(Widget sender) {
-        // Do nothing
-      }
-
-      public void onLostFocus(Widget sender) {
-        popup.hide();
-      }
-
-    });
-    listener = new MyListener();
-    button.addKeyboardListener(listener);
+  public SelectablePopupButton(ClippedImage upImage, ClippedImage downImage,
+      SelectablePopup popup) {
+    this(popup);
+    getUp().setFace(upImage);
+    getDown().setFace(downImage);
   }
 
   public void addChangeListener(ChangeListener listener) {
     popup.addChangeListener(listener);
-  }
-
-  /**
-   * Gets the push button.
-   * 
-   * @return the push button
-   */
-  public ComplexButton getButton() {
-    return button;
   }
 
   /**
@@ -132,32 +107,75 @@ public class SelectablePopupButton extends Composite implements
    */
   public SelectablePopup getPopup() {
     return popup;
-  };
+  }
 
   public void removeChangeListener(ChangeListener listener) {
     popup.removeChangeListener(listener);
   }
 
   /**
-   * Sets the optional item controller. If a <code>ItemController</code> is
-   * given a <code>SelectablePopup</code> subtype it does not expect, runtime
-   * behavior may be somewhat strange.
+   * Sets the optional item controller. Once a item controller is installed, the
+   * item controller will be responsible for displaying the popup.
+   * 
+   * <p>
+   * Note, if a <code>ItemController</code> is given a
+   * <code>SelectablePopup</code> subtype it does not expect, runtime behavior
+   * may be somewhat strange.
    * 
    * @param controller controller to set
    */
   public void setController(final ItemController controller) {
     this.controller = controller;
+  };
+
+  /**
+   * Sets the current popup.
+   * 
+   * @param popup popup to set
+   */
+  public void setPopup(SelectablePopup popup) {
+    popup = this.popup;
   }
 
   /**
    * Shows the popup.
    */
   public void showPopup() {
-    button.setPressed(true);
+    setPressed(true);
     if (controller != null) {
-      controller.showBelow(popup, listener.accum.toString(), button);
+      controller.showBelow(popup, listener.accum.toString(), this);
     } else {
       popup.showBelow(this);
     }
+  }
+
+  private void addDefaultListeners() {
+    // Toggles menu based on button click.
+    addClickListener(new ClickListener() {
+      public void onClick(Widget sender) {
+        if (isPressed()) {
+          showPopup();
+        } else {
+          popup.hide();
+        }
+      }
+    });
+
+    // If the popup is closed for any reason, set the button to be depressed.
+    popup.addPopupListener(new PopupListener() {
+      public void onPopupClosed(PopupPanel sender, boolean autoClosed) {
+        setPressed(false);
+        listener.accum.setLength(0);
+      }
+    });
+
+    // If the button loses focus, hide the popup.
+    addFocusListener(new FocusListenerAdapter() {
+      public void onLostFocus(Widget sender) {
+        popup.hide();
+      }
+    });
+    listener = new MyListener();
+    addKeyboardListener(listener);
   }
 }
