491d4f220d853a70fb5ead58d190e484169d1c66
rjrjr@google.com <rjrjr@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2008-10-31 22:29:32 +0000

    vis dialog test now checks for dragging override
    git-svn-id: http://google-web-toolkit.googlecode.com/svn/branches/1_6_events@3928 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- reference/code-museum/src/com/google/gwt/museum/client/defaultmuseum/VisualsForDialogBox.java
+++ reference/code-museum/src/com/google/gwt/museum/client/defaultmuseum/VisualsForDialogBox.java
@@ -16,10 +16,13 @@
 package com.google.gwt.museum.client.defaultmuseum;
 
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
 import com.google.gwt.museum.client.common.AbstractIssue;
+import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.DialogBox;
 import com.google.gwt.user.client.ui.FlexTable;
-import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
 import java.util.HashMap;
@@ -30,80 +33,125 @@ import java.util.Map;
  */
 public class VisualsForDialogBox extends AbstractIssue {
 
-  /**
-   * The main grid used for layout.
-   */
-  private FlexTable layout = null;
-
   enum Event {
     mouseDown, mouseEnter, mouseLeave, mouseMove, mouseUp
   }
 
-  final Map<Event, Element> eventToElement = new HashMap<Event, Element>();
+  private final class VisibleDialogBox extends DialogBox {
+    private FlexTable layout = null;
+
+    private final Map<Event, Element> eventToElement =
+        new HashMap<Event, Element>();
+
+    private Boolean defeatDragging = false;
+
+    public VisibleDialogBox() {
+      this(false);
+    }
+
+    public VisibleDialogBox(boolean autoHide) {
+      this(autoHide, true);
+    }
+
+    public VisibleDialogBox(boolean autoHide, boolean modal) {
+      super(autoHide, modal);
+      layout = new FlexTable();
+      layout.setCellPadding(3);
+      layout.setBorderWidth(2);
+      layout.setHTML(0, 0, "<b>Event</b>");
+      layout.setHTML(0, 1, "<b>Status</b>");
+
+      for (Event e : Event.values()) {
+        eventToElement.put(e, addResultRow(e.name()));
+      }
+      add(layout);
+    }
+
+    @Override
+    public void onMouseDown(Widget sender, int x, int y) {
+      pass(Event.mouseDown);
+      if (!defeatDragging) {
+        super.onMouseDown(sender, x, y);
+      }
+    }
+
+    @Override
+    public void onMouseEnter(Widget sender) {
+      pass(Event.mouseEnter);
+      if (!defeatDragging) {
+        super.onMouseEnter(sender);
+      }
+    }
+
+    @Override
+    public void onMouseLeave(Widget sender) {
+      pass(Event.mouseLeave);
+      if (!defeatDragging) {
+        super.onMouseLeave(sender);
+      }
+    }
+
+    @Override
+    public void onMouseMove(Widget sender, int x, int y) {
+      pass(Event.mouseMove);
+      if (!defeatDragging) {
+        super.onMouseMove(sender, x, y);
+      }
+    }
+
+    @Override
+    public void onMouseUp(Widget sender, int x, int y) {
+      pass(Event.mouseUp);
+      super.onMouseUp(sender, x, y);
+    }
+
+    public void setDefeatDragging(Boolean defeatDragging) {
+      this.defeatDragging = defeatDragging;
+    }
+
+    private Element addResultRow(String eventName) {
+      int row = layout.getRowCount();
+      layout.setHTML(row, 0, eventName);
+      layout.setHTML(row, 1, "<span style='color:red'>?</span>");
+      Element cell = layout.getCellFormatter().getElement(row, 1);
+      return cell;
+    }
+
+    private void pass(Event event) {
+      eventToElement.get(event).setInnerHTML(
+          "<span style='color:green'>pass</span>");
+    }
+  }
+
+  private CheckBox defeatDragging;
 
   @Override
   public Widget createIssue() {
-    layout = new FlexTable();
-    layout.setCellPadding(3);
-    layout.setBorderWidth(2);
-    layout.setHTML(0, 0, "<b>Event</b>");
-    layout.setHTML(0, 1, "<b>Status</b>");
+    final VisibleDialogBox dialog = showVisibleDialog();
 
-    for (Event e : Event.values()) {
-      eventToElement.put(e, addResultRow(e.name()));
-    }
-
-    final DialogBox dialog = new DialogBox() {
-
-      @Override
-      public void onMouseDown(Widget sender, int x, int y) {
-        pass(Event.mouseDown);
-        super.onMouseDown(sender, x, y);
+    defeatDragging = new CheckBox("Defeat dragging");
+    defeatDragging.addValueChangeHandler(new ValueChangeHandler<Boolean>() {
+      public void onValueChange(ValueChangeEvent<Boolean> event) {
+        dialog.setDefeatDragging(event.getValue());
       }
-
-      @Override
-      public void onMouseEnter(Widget sender) {
-        pass(Event.mouseEnter);
-        super.onMouseEnter(sender);
-      }
-
-      @Override
-      public void onMouseLeave(Widget sender) {
-        pass(Event.mouseLeave);
-        super.onMouseLeave(sender);
-      }
-
-      @Override
-      public void onMouseMove(Widget sender, int x, int y) {
-        pass(Event.mouseMove);
-        super.onMouseMove(sender, x, y);
-      }
-
-      @Override
-      public void onMouseUp(Widget sender, int x, int y) {
-        pass(Event.mouseUp);
-        super.onMouseUp(sender, x, y);
-      }
-
-    };
-    dialog.setModal(false);
-    dialog.setText("Click here");
-    dialog.add(layout);
+    });
     
-    dialog.center();
-
-    return new HTML() {
+    SimplePanel panel = new SimplePanel() {
       @Override
-      protected void onDetach() {
+      protected void onUnload() {
         dialog.hide();
       }
     };
+    panel.add(defeatDragging);
+    
+    return panel;
   }
 
   @Override
   public String getInstructions() {
     return "Confirm that the events listed happen when you click on the "
-        + "dialog's caption.";
+        + "dialog's caption. Confirm that dragging is defeated when the "
+        + "checkbox is selected.";
   }
 
   @Override
@@ -115,17 +163,14 @@ public class VisualsForDialogBox extends AbstractIssue {
   public boolean hasCSS() {
     return false;
   }
-  
-  private Element addResultRow(String eventName) {
-    int row = layout.getRowCount();
-    layout.setHTML(row, 0, eventName);
-    layout.setHTML(row, 1, "<span style='color:red'>?</span>");
-    Element cell = layout.getCellFormatter().getElement(row, 1);
-    return cell;
-  }
 
-  private void pass(Event event) {
-    eventToElement.get(event).setInnerHTML(
-        "<span style='color:green'>pass</span>");
+  private VisibleDialogBox showVisibleDialog() {
+    final VisibleDialogBox dialog = new VisibleDialogBox();
+    dialog.setModal(false);
+    dialog.setText("I'm a dialog");
+
+    dialog.center();
+
+    return dialog;
   }
 }
