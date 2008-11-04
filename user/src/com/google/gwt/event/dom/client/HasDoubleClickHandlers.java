2bd31257a6e83fecc5832f00e8e3b40e66c2dad0
ecc@google.com <ecc@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2008-11-04 22:03:03 +0000

    Fixed weird package indentation problem.
    git-svn-id: http://google-web-toolkit.googlecode.com/svn/branches/1_6_clean_events@3948 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/event/dom/client/HasDoubleClickHandlers.java
+++ user/src/com/google/gwt/event/dom/client/HasDoubleClickHandlers.java
@@ -12,7 +12,8 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- */package com.google.gwt.event.dom.client;
+ */
+package com.google.gwt.event.dom.client;
 
 import com.google.gwt.event.shared.HandlerRegistration;
 
