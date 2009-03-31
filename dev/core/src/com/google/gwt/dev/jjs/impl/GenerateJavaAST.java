64030ecad9801f548de37221465edd851ab4c05d
jlabanca@google.com <jlabanca@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2009-03-31 18:15:47 +0000

    Added imports to GenerateJavaAST so it would compile.
    Patch by: jlabanca
    Review by: spoon (desk)
    
    
    
    git-svn-id: http://google-web-toolkit.googlecode.com/svn/branches/snapshot-2009.03.30-r5111@5138 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- dev/core/src/com/google/gwt/dev/jjs/impl/GenerateJavaAST.java
+++ dev/core/src/com/google/gwt/dev/jjs/impl/GenerateJavaAST.java
@@ -191,10 +191,12 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 import java.util.TreeSet;
 
 /**
