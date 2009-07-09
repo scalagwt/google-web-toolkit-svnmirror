cab4fba0429707e10fe50c7cd327212b26b94263
jgw@google.com <jgw@google.com@8db76d5a-ed1c-0410-87a9-c151d255dfc7>
2009-07-09 16:30:45 +0000

    Merging /trunk c5691, c5696 into this branch.
    git-svn-id: http://google-web-toolkit.googlecode.com/svn/branches/snapshot-2009.06.16-r5570@5699 8db76d5a-ed1c-0410-87a9-c151d255dfc7

--- user/src/com/google/gwt/user/client/impl/HistoryImpl.java
+++ user/src/com/google/gwt/user/client/impl/HistoryImpl.java
@@ -38,16 +38,17 @@ import com.google.gwt.event.shared.HasHandlers;
 public class HistoryImpl implements HasValueChangeHandlers<String>,
     HasHandlers {
 
-  static boolean updateHashOnIE6 = true;
-
   /**
    * Sets whether the IE6 history implementation will update the URL hash when
    * creating a new item. This should be used only for applications with large
    * DOM structures that are suffering from performance problems when creating
    * a new history item on IE6 and 7.
+   *
+   * @deprecated This is no longer necessary, as the underlying performance
+   * problem has been solved. It is now a no-op.
    */
+  @Deprecated
   public static void setUpdateHashOnIE6(boolean updateHash) {
-    HistoryImpl.updateHashOnIE6 = updateHash;
   }
 
   public static native String getToken() /*-{
